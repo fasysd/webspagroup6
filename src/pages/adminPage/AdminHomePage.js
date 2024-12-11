bb4292af24a1aed2c2f74238123c167de543eb78
fasysd <manhhung25017@gmail.com>
2024-12-11 15:19:03 +0700

    II comit

--- src/pages/adminPage/AdminHomePage.js
+++ src/pages/adminPage/AdminHomePage.js
@@ -50,43 +50,45 @@ const AdminHomePage = () => {
 
     return(
         <div className="Spa__information">
-        <header className="Spa_Header">
-            <img src={informationOfSpa.logo} className="Spa_Header__logo"/>
-            <div className="Spa_Header__information">
-                <h1 className="Spa_Header__information__name">{informationOfSpa.name}</h1>
-                <h2 className="Spa_Header__information__rating">{informationOfSpa.userRating}</h2>
-                <h2 className="Spa_Header__information__description">{informationOfSpa.description}</h2>
-            </div>
-        </header>
-        <div className="Spa__Services">
-            <h1>Danh sách dịch vụ</h1>
-            <ul>
-                {informationOfSpa.services.map( item => {
-                    return(
-                        <li>
-                            <button onClick={()=>{alert( "service information")}}>
-                                <h1 className="Spa__Services__name">{item.name}</h1>
-                            </button>
-                        </li>
-                    )
-                })}
+            <header className="Spa_Header">
+                <img src={informationOfSpa.logo} className="Spa_Header__logo"/>
+                <div className="Spa_Header__information">
+                    <h1 className="Spa_Header__information__name">{informationOfSpa.name}</h1>
+                    <h2 className="Spa_Header__information__rating">{informationOfSpa.userRating}</h2>
+                    <h2 className="Spa_Header__information__description">{informationOfSpa.description}</h2>
+                </div>
+            </header>
+            <ul className="Spa_Content">
+                <div className="Spa__Services">
+                    <h1>Danh sách dịch vụ</h1>
+                    <ul>
+                        {informationOfSpa.services.map( item => {
+                            return(
+                                <li>
+                                    <button onClick={()=>{alert( "service information")}}>
+                                        <h1 className="Spa__Services__name">{item.name}</h1>
+                                    </button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className="Spa__Staffs">
+                    <h1>Danh sách nhân viên</h1>
+                    <ul>
+                        {informationOfSpa.staff.map( item => {
+                            return(
+                                <li>
+                                    <button onClick={()=>{alert( "Staff information")}}>
+                                        <img src={item.avatar} className="Spa__Staffs__avatar"></img>
+                                        <h1 className="Spa__Staffs__name">{item.name}</h1>
+                                    </button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </ul>
-        </div>
-        <div className="Spa__Staffs">
-            <h1>Danh sách nhân viên</h1>
-            <ul>
-                {informationOfSpa.staff.map( item => {
-                    return(
-                        <li>
-                            <button onClick={()=>{alert( "Staff information")}}>
-                                <img src={item.avatar} className="Spa__Staffs__avatar"></img>
-                                <h1 className="Spa__Staffs__name">{item.name}</h1>
-                            </button>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
         </div>
     )
 }
